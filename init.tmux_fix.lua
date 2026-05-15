20cf779e58bf30997537b72601b58d6407f4fc20
EC2 Default User <ec2-user@ip-10-104-124-45.evoforge.org>
2026-05-15 15:06:11 +0000

    Fixing recent neovim tmux interaction.

---
+++ init.tmux_fix.lua
@@ -0,0 +1,13 @@
+vim.g.clipboard = {
+    name = 'myClipboard',
+    copy = {
+        ['+'] = { 'tmux', 'load-buffer', '-' },
+        ['*'] = { 'tmux', 'load-buffer', '-' },
+    },
+    paste = {
+        ['+'] = { 'tmux', 'save-buffer', '-' },
+        ['*'] = { 'tmux', 'save-buffer', '-' },
+    },
+    cache_enabled = 1,
+}
+
