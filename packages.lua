3582fe09c7504146fbd4ea567208367bad390ab0
Chris Nelson <ID+foo3356257793@users.noreply.github.com>
2026-05-21 21:51:22 -0400

    zen mode package

--- packages.lua
+++ packages.lua
@@ -12,7 +12,7 @@ vim.pack.add({
   github("lervag/vimtex"),
   github("kien/rainbow_parentheses.vim"),
   github("christoomey/vim-tmux-navigator"),
-  github("Dru89/vim-adventurous"),
+  github("folke/zen-mode.nvim"),
 
   {
     src     = github("averms/black-nvim"),
@@ -29,26 +29,3 @@ vim.keymap.set("n", "<C-j>",  "<cmd>TmuxNavigateDown<cr>",     { silent = true, 
 vim.keymap.set("n", "<C-k>",  "<cmd>TmuxNavigateUp<cr>",       { silent = true, desc = "Navigate up"       })
 vim.keymap.set("n", "<C-l>",  "<cmd>TmuxNavigateRight<cr>",    { silent = true, desc = "Navigate right"    })
 vim.keymap.set("n", "<C-\\>", "<cmd>TmuxNavigatePrevious<cr>", { silent = true, desc = "Navigate previous" })
-
--- ============================================================================
--- COLORSCHEME
--- ============================================================================
-
-local theme_group = vim.api.nvim_create_augroup("ThemeOverrides", { clear = true })
-
-vim.api.nvim_create_autocmd("ColorScheme", {
-  group    = theme_group,
-  pattern  = "*",
-  callback = function()
-    local groups = {
-      "Normal", "NormalNC", "NormalFloat",
-      "SignColumn", "LineNr", "CursorLineNr",
-      "Folded",
-    }
-    for _, group in ipairs(groups) do
-      vim.api.nvim_set_hl(0, group, { bg = "NONE" })
-    end
-  end,
-})
-
-vim.cmd("colorscheme adventurous")
